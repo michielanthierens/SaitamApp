505aa0deb32551f3cc3c36675f0d3a26fe4aaa77
michiel.anthierens <michiel.anthierens@student.howest.be>
2023-06-19 18:54:15 +0200

    basic app ready

--- app/src/main/java/be/howest/ti/saitamapp/ui/tabs/Overview.kt
+++ app/src/main/java/be/howest/ti/saitamapp/ui/tabs/Overview.kt
@@ -1,5 +1,7 @@
 package be.howest.ti.saitamapp.ui.tabs
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -16,8 +18,12 @@ import be.howest.ti.saitamapp.data.ProgressDatabase
 import be.howest.ti.saitamapp.model.ProgressDay
 import be.howest.ti.saitamapp.ui.theme.lightYellow
 import be.howest.ti.saitamapp.R
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun Overview(
     database: ProgressDatabase,
@@ -47,6 +53,7 @@ fun Overview(
     }
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun ProgressDayItem(progressDay: ProgressDay) {
     Card(
@@ -61,13 +68,16 @@ fun ProgressDayItem(progressDay: ProgressDay) {
             verticalAlignment = Alignment.CenterVertically
         ) {
             val iconRes = when {
-                progressDay.progress <= 25 -> R.drawable.saitama1
-                progressDay.progress <= 50 -> R.drawable.saitama2
-                progressDay.progress <= 75 -> R.drawable.saitama3
-                else -> R.drawable.saitama4
+                progressDay.progress >= 75 -> R.drawable.saitama4
+                progressDay.progress >= 50 -> R.drawable.saitama3
+                progressDay.progress >= 25 -> R.drawable.saitama2
+                else -> R.drawable.saitama1
             }
+            val formatter = DateTimeFormatter.ofPattern("dd MMM", Locale.getDefault())
+            val date = LocalDate.parse(progressDay.date)
+            val formattedDate = date.format(formatter)
             Text(
-                text = progressDay.date,
+                text = formattedDate,
                 color = Color.Black,
                 style = MaterialTheme.typography.subtitle1,
                 modifier = Modifier.weight(1f)
